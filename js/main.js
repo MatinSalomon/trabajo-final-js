71abc49452f4a3485d565941ba9fabb4eb14e7b9
Martin Salomon <salomonmartin21@gmail.com>
2022-07-20 15:06:28 -0300

    agregando js 2

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const peliculasAccion = ["Memory", "Morbius", "Nadie", "Mortal Kombat"];
+const peliculasRomance = ["La ciudad perdida", "Emma", "your name", "una voz silencionsa", "amor de gata"];
+const peliculasTodas = peliculasAccion.concat(peliculasRomance);
+
+function BuscarPelicula() {
+    let nombrePlicula = prompt("ingrese el nombre de la pelicula que quiere ver")
+    for(let i = 0; i = peliculasTodas,length; i++){
+       if(nombrePlicula == )
+}
