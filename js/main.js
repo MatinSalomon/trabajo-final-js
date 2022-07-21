c6110166c92f3faff19b300b998a429a6a971e68
Martin Salomon <salomonmartin21@gmail.com>
2022-07-21 15:09:28 -0300

    21/07

--- js/main.js
+++ js/main.js
@@ -2,8 +2,17 @@ const peliculasAccion = ["Memory", "Morbius", "Nadie", "Mortal Kombat"];
 const peliculasRomance = ["La ciudad perdida", "Emma", "your name", "una voz silencionsa", "amor de gata"];
 const peliculasTodas = peliculasAccion.concat(peliculasRomance);
 
-function BuscarPelicula() {
-    let nombrePlicula = prompt("ingrese el nombre de la pelicula que quiere ver")
-    for(let i = 0; i = peliculasTodas,length; i++){
-       if(nombrePlicula == )
-}
+function Buscador(){
+    let nombrePelicula = prompt("ingrese el nombre de la pelicula que quiere ver: ")
+    for(let i = 0; i <= peliculasTodas.length; i++){
+        if(peliculasTodas[i] == nombrePelicula){
+            alert(peliculasTodas[i])
+           break 
+        }else if(peliculasTodas[i] != nombrePelicula){
+            alert("no encontre tu peli")
+            break
+        }  
+     }
+    
+ }
+ Buscador();
