878c7d822e909f2bba510b29042031bf45197923
Martin Salomon <salomonmartin21@gmail.com>
2022-07-25 23:14:43 -0300

    25-07-22

--- js/main.js
+++ js/main.js
@@ -1,18 +1,65 @@
-const peliculasAccion = ["Memory", "Morbius", "Nadie", "Mortal Kombat"];
-const peliculasRomance = ["La ciudad perdida", "Emma", "your name", "una voz silencionsa", "amor de gata"];
+class pelicula{
+    constructor (nombre, genero, duracion, precio) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.duracion = duracion;
+        this.precio = precio;
+    }
+    sumarIva(){
+        this.precio *= 1.21;
+    }
+
+}
+
+const peliculasAccion = [
+    new pelicula ("memory", "accion", 1.54, 150),
+    new pelicula ("morbius", "accion", 1.44, 200),
+    new pelicula ("mortal Kombat", "accion", 1.50, 100)
+]
+
+for ( const pelicula of peliculasAccion){
+    pelicula.sumarIva();
+}
+
+const peliculasRomance =  [
+    new pelicula ("your name", "romance", 1.52, 500),
+    new pelicula("una voz silenciosa", "romance", 2.09, 350),
+    new pelicula("amor de gata", "romance", 1.44, 200)
+]
+
+for ( const pelicula of peliculasRomance){
+    pelicula.sumarIva();
+}
+
 const peliculasTodas = peliculasAccion.concat(peliculasRomance);
 
-function Buscador(){
-    let nombrePelicula = prompt("ingrese el nombre de la pelicula que quiere ver: ")
-    for(let i = 0; i <= peliculasTodas.length; i++){
-        if(peliculasTodas[i] == nombrePelicula){
-            alert(peliculasTodas[i])
-           break 
-        }else if(peliculasTodas[i] != nombrePelicula){
-            alert("no encontre tu peli")
+function buscarPeli(){
+    let peliBuscar = prompt("que pelicula buscas: ")
+    for(const pelicula of peliculasTodas){
+        if(pelicula.nombre == peliBuscar){
+            console.log("El nombre de la pelicula que buscas es: " + pelicula.nombre)
+            console.log("El genero de la pelicula que buscas es: " + pelicula.genero)
+            console.log("La duracion de la pelicula que buscas es de: " + pelicula.duracion)
+            console.log("El precio de la pelicula que buscas es de : " + pelicula.precio)
             break
-        }  
-     }
-    
- }
- Buscador();
+        }else if (pelicula.nombre != peliBuscar){
+            console.log("lo siento no tenemos esa pelicula")
+        }
+    }
+}
+
+function buscarSioNo(){
+let buscar = prompt("quieres buscar un pelicula si o no: ")
+
+if (buscar == "si") {
+    buscarPeli();
+} else {
+    for(const pelicula of peliculasTodas){
+        console.log("El nombre de la pelicula que buscas es: " + pelicula.nombre)
+    }
+}
+alert("En consola puedes ver todo nuestro catalago de peliculas")
+}
+
+
+buscarSioNo()
