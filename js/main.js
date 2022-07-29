8ef733c098919d3fc24af5cef59ec21fc80ae7f3
Martin Salomon <salomonmartin21@gmail.com>
2022-07-29 17:57:06 -0300

    arreglando bug

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/**
+ * clase para las peliculas
+ */
 class pelicula{
     constructor (nombre, genero, duracion, precio) {
         this.nombre = nombre;
@@ -10,7 +13,9 @@ class pelicula{
     }
 
 }
-
+/**
+ * array que almacena la peliculas de accion
+ */
 const peliculasAccion = [
     new pelicula ("memory", "accion", 1.54, 150),
     new pelicula ("morbius", "accion", 1.44, 200),
@@ -21,6 +26,9 @@ for ( const pelicula of peliculasAccion){
     pelicula.sumarIva();
 }
 
+/**
+ * array que almacena la peliculas de romance
+ */
 const peliculasRomance =  [
     new pelicula ("your name", "romance", 1.52, 500),
     new pelicula("una voz silenciosa", "romance", 2.09, 350),
@@ -31,8 +39,14 @@ for ( const pelicula of peliculasRomance){
     pelicula.sumarIva();
 }
 
+/**
+ * array que almacena todas la peliculas
+ */
 const peliculasTodas = peliculasAccion.concat(peliculasRomance);
 
+/**
+ * Comprueba si la pelicula que ingresa el usuario esta en el array @peliculasTodas
+ */
 function buscarPeli(){
     let peliBuscar = prompt("que pelicula buscas: ")
     for(const pelicula of peliculasTodas){
@@ -41,25 +55,23 @@ function buscarPeli(){
             console.log("El genero de la pelicula que buscas es: " + pelicula.genero)
             console.log("La duracion de la pelicula que buscas es de: " + pelicula.duracion)
             console.log("El precio de la pelicula que buscas es de : " + pelicula.precio)
-            break
-        }else if (pelicula.nombre != peliBuscar){
-            console.log("lo siento no tenemos esa pelicula")
-        }
+        } 
     }
+    alert("Puedes ver el resultado en la consola")
 }
 
+/**
+ *  Pregunta al usuario si quiere busca una pelicula o no si la repuesta es no muestra el catalogo de peliculas en la consola y si es si retorna la funcion buscarPeli
+ * @returns la funcion buscarPeli
+ */
 function buscarSioNo(){
 let buscar = prompt("quieres buscar un pelicula si o no: ")
-
 if (buscar == "si") {
-    buscarPeli();
-} else {
-    for(const pelicula of peliculasTodas){
-        console.log("El nombre de la pelicula que buscas es: " + pelicula.nombre)
-    }
+    return buscarPeli();
+}else{
+    alert("En consola puedes ver todo nuestro catalago de peliculas con su duracion y genero")
+    console.log(peliculasTodas)
 }
-alert("En consola puedes ver todo nuestro catalago de peliculas")
 }
-
 
 buscarSioNo()
